2d025aa341f9cb867c369c85069fd6be8d4b5af1
Mayank Wilkenson <89447671+Shad33-1437@users.noreply.github.com>
2021-09-06 10:56:03 +0530

    Add files via upload

---
+++ evenodd.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter a no \n");
+    scanf("%d",&n);
+    if(n%2==0)
+    printf("Its an even no.");
+    else
+    printf("Its an odd no.");
+}
